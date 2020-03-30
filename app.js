ae724968af2fbbebe2fb7703e8f5fce32699f625
ahsanmubariz <ahsan.mubariz@gmail.com>
2020-03-30 23:32:25 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-var cheerio = require('cheerio')
-var request = require('request-promise')
-var express = require("express")
-var port = process.env.PORT || 3000;
-var app = express()
+const request = require('request-promise')
+const express = require("express")
+const port = process.env.PORT || 3000;
+const cron = require('node-cron')
+const app = express()
 
 app.listen(port, () => {
  console.log("Server running on port 3000, created by ahsan mubariz");
 });
 
 let datayya, ODP, pdp, positif, rujukan
-var cron = require('node-cron');
+
  
-cron.schedule('59 23 * * *', () => {
+cron.schedule('* */5 * * *', () => {
     request({
         url: 'https://covid19.sulselprov.go.id/',
         "rejectUnauthorized": false
@@ -36,10 +36,10 @@ request({
     pdp = JSON.parse(data.match(/var PDP =.*?\n/gm).toString().replace('var PDP = ','').replace(/'/g,'').replace(/;/g,''));
     positif = JSON.parse(data.match(/var POSITIF =.*?\n/gm).toString().replace('var POSITIF = ','').replace(/'/g,'').replace(/;/g,''));
     rujukan = JSON.parse(data.match(/var rujukan =.*?\n/gm).toString().replace('var rujukan = ','').replace(/'/g,'').replace(/;/g,''));
-
 }, function(err) {
     console.log(err);
 });
+
 app.get("/", (req, res, next) => {
     res.json({createdby:"2020- ahsan mubariz",datasource:'https://covid19.sulselprov.go.id/',documentation:'read https://github.com/ahsanmubariz/covid19sulselapi.git'});
 });
@@ -56,3 +56,21 @@ app.get("/about", async (req, res, next) => {
     await res.json({copyright:"2020, ahsan mubariz",datasource:'https://covid19.sulselprov.go.id/'});
 });
 
+app.get("/statistik", async (req, res, next) => {
+    var positifCount = {},e;
+    for (var i = 0,l=positif.length; i < l; i++) { 
+        e = positif[i];
+        positifCount[e.properties.title] = (positifCount[e.properties.title] || 0) + 1;
+    }
+    var odpCount = {},e;
+    for (var i = 0,l=ODP.length; i < l; i++) { 
+        e = ODP[i];
+        odpCount[e.properties.title] = (odpCount[e.properties.title] || 0) + 1;
+    }
+    var pdpCount = {},e;
+    for (var i = 0,l=pdp.length; i < l; i++) { 
+        e = pdp[i];
+        pdpCount[e.properties.title] = (pdpCount[e.properties.title] || 0) + 1;
+    }
+    await res.json({odp:odpCount,positif:positifCount,pdp:pdpCount})
+});
